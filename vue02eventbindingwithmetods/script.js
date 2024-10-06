827a8701b3ebdedfde9ced78f10a05a2c18b742b
Web-Mastermind <azeagent@gmail.com>
2024-10-06 14:40:14 +0400

    event binding with methods

---
+++ vue02eventbindingwithmetods/script.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp(
+  {
+    data() {
+      return {
+        counter: 0,
+      };
+    },
+    methods: {
+      add(number) {
+        this.counter = this.counter + number;
+      },
+      reduce(number) {
+        this.counter -= number;
+      }
+    },
+  });
+
+app.mount('#frontend');
